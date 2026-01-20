024e8ae516601dcb51dd66ed72125cdbf544efc6
tristanDurden <thelossofsight@gmail.com>
2026-01-20 20:15:49 +0400

    fixed dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,6 +3,9 @@ import NavBar from "../components/NavBar";
 import { useSession } from "next-auth/react";
 import ActivityTable from "../components/ActivityTable";
 import Tabs from "../components/Tabs";
+import { Button } from "@/components/ui/button";
+import { AddDefaultHabit } from "@/lib/types";
+import HabitDialog from "../components/HabitDialog";
 
 export default function DashboardPage() {
   const { data: session } = useSession();
@@ -10,6 +13,14 @@ export default function DashboardPage() {
   return (
     <div className="min-h-screen flex flex-col">
       <NavBar />
+      <div className="flex flex-row gap-5 items-center py-3">
+        <h1>
+          Welcome, {session?.user?.name || "User"}! Your habit journey starts
+          here 💪
+        </h1>
+
+        <HabitDialog mode="add" habit={AddDefaultHabit} />
+      </div>
       <main className="flex-1 p-6">
         <Tabs />
       </main>
